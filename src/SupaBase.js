7aaad263e4360ec5685fbba2380403c9f07577e1
ab-h-i-n <abhinshelly@gmail.com>
2024-02-28 19:54:27 +0530

    .

--- src/SupaBase.js
+++ src/SupaBase.js
@@ -1,4 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
+
 const supabaseUrl = 'https://rdflgkoeugtqoxnqhliv.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZmxna29ldWd0cW94bnFobGl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg5Njg2MzUsImV4cCI6MjAyNDU0NDYzNX0.H4GG1YKNHWfW7uioRRDGL01rA-HnpCQt6BM0tJA_TR0'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+    localStorage: false  // Disable local storage
+})
+
