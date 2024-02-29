edbd0892016e8cd8b21b5f9bf07ed9a129f68a7e
ab-h-i-n <abhinshelly@gmail.com>
2024-02-29 21:30:24 +0530

    cleaned

--- src/SignupLogin.js
+++ src/SignupLogin.js
@@ -3,65 +3,71 @@ import secureLocalStorage from "react-secure-storage";
 
 const SignupLogin = (form) => {
 
-    // signup
-    const handleSignUp = async () => {
-        try {
-            console.log(form);
+  // signup
+  const handleSignUp = async () => {
+    try {
+      console.log(form);
 
-            const { data, error } = await supabase.auth.signUp({
-                email: form.email,
-                password: form.password,
-                options: {
-                    data: {
-                        full_name: form.options.data.full_name,
-                    },
-                },
-            });
+      const { data, error } = await supabase.auth.signUp({
+        email: form.email,
+        password: form.password,
+        options: {
+          data: {
+            full_name: form.options.data.full_name,
+          },
+        },
+      });
 
-            if (error) {
-                alert(error);
-            }
+      if (error) {
+        alert(error);
+        return false;
+      }
 
-            secureLocalStorage.setItem("user", data);
+      secureLocalStorage.setItem("user", data);
 
-            console.log(data);
+      console.log(data);
 
-            alert("Signed Up successfully!");
+      alert("Signed Up successfully!");
+      return true;
 
-        } catch (error) {
-            alert(error);
-        }
+    } catch (error) {
+      alert(error);
+      return false;
     }
+  }
 
-    // Login
-    const handleLogIn = async () => {
-        try {
-            console.log(form);
+  // Login
+  const handleLogIn = async () => {
+    try {
+      console.log(form);
 
-            const { data, error } = await supabase.auth.signInWithPassword({
-                email: form.email,
-                password: form.password,
-            });
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: form.email,
+        password: form.password,
+      });
 
-            if (error) {
-                alert(error);
-            }
+      if (error) {
+        alert(error);
+        return false;
+      }
 
-            secureLocalStorage.setItem("user", data);
+      secureLocalStorage.setItem("user", data);
 
-            console.log(data);
+      console.log(data);
 
-            alert('User Signed Up successfully!');
+      alert('User Signed Up successfully!');
+      return true;
 
-        } catch (error) {
-            alert(error);
-        }
-    };
+    } catch (error) {
+      alert(error);
+      return false;
+    }
+  };
 
-    return {
-        handleLogIn,
-        handleSignUp
-    };
+  return {
+    handleLogIn,
+    handleSignUp
+  };
 };
 
 export default SignupLogin;
